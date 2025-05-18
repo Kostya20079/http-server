c24bbc3cd5376ccde32c77a8d4be7130036efb6f
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2025-05-18 14:08:23 +0200

    Init project

---
+++ src/main/java/com/httpserver/HttpServer.java
@@ -0,0 +1,15 @@
+package com.httpserver;
+
+/**
+ *
+ * Driver Class for the Http Server
+ *
+ **/
+
+public class HttpServer {
+    public static void main(String[] args) {
+
+        System.out.println("Server starting...") ;
+
+    }
+}
