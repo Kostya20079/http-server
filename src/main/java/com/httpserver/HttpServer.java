9225b9445d681e3e22fd632771d03e44df4527df
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2025-05-27 19:16:53 +0200

    Add ServerListenerThread and Add Logging

--- src/main/java/com/httpserver/HttpServer.java
+++ src/main/java/com/httpserver/HttpServer.java
@@ -2,12 +2,11 @@ package com.httpserver;
 
 import com.httpserver.config.Configuration;
 import com.httpserver.config.ConfigurationManager;
+import com.httpserver.core.ServerListenerThread;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -16,45 +15,25 @@ import java.net.Socket;
  **/
 
 public class HttpServer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
     public static void main(String[] args) {
 
-        System.out.println("Server starting...") ;
+        LOGGER.info("Server starting...") ;
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration config = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("Reading at port: " + config.getPort());
-        System.out.println("Using WebRoot: " + config.getWebroot());
+        LOGGER.info("Reading at port: " + config.getPort());
+        LOGGER.info("Using WebRoot: " + config.getWebroot());
+
 
         try {
-            ServerSocket serverSocket = new ServerSocket(config.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html>" +
-                    "<head><title>Java HTTP Server</title></head>" +
-                    "<body><h1>This page was served using Java HTTP Server</h1></body>" +
-                    "</html>";
-
-            final String CRLF = "\r\n";
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF + // Status line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-length: " + html.getBytes().length + CRLF + // HEADER
-                        CRLF +
-                        html +
-                        CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
+            ServerListenerThread serverListenerThread = new ServerListenerThread(config.getPort(), config.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
